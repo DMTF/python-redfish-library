bdcce0498dc21a50a3c0ea43998fdf99ac38dd33
Hervé Beraud <hberaud@redhat.com>
2019-05-17 15:27:42 +0200

    Introduce unittests on rest v1
    Introduce some tests on:
    - redfish_client
    - HttpClient

---
+++ tests/test_redfish.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+import unittest
+
+from redfish.rest.v1 import redfish_client
+from redfish.rest.v1 import HttpClient
+from redfish.rest.v1 import RetriesExhaustedError
+
+
+class TestRedFishClient(unittest.TestCase):
+    def test_redfish_client(self):
+        base_url = 'http://foo.bar'
+        username='rstallman'
+        password='123456'
+        default_prefix='/custom/redfish/v1/'
+        sessionkey='fg687glgkf56vlgkf'
+        capath='/path/to/the/dir'
+        cafile='filename.test'
+        timeout=666
+        max_retry=42
+        # NOTE(hberaud) the client try to connect when we initialize the
+        # http client object so we need to catch the retries exception first.
+        # In a second time we need to mock the six.http_client to simulate
+        # server responses and do some other tests
+        with self.assertRaises(RetriesExhaustedError):
+            client = redfish_client(
+                base_url=base_url,
+            )
+            # Check the object type
+            self.assertTrue(isinstance(client, HttpClient))
+            # Check the object attributes values.
+            # Here we check if the client object is properly initialized
+            self.assertEqual(client.base_url, base_url)
+            self.assertEqual(client.username, username)
+            self.assertEqual(client.password, password)
+            self.assertEqual(client.default_prefix, default_prefix)
+            self.assertEqual(client.sessionkey, sessionkey)
+            self.assertEqual(client.capath, capath)
+            self.assertEqual(client.cafile, cafile)
+            self.assertEqual(client.timeout, timeout)
+            self.assertEqual(client.max_retry, max_retry)

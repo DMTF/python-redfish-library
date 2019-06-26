980e193c5b156f8e445cf7324f819947c0b96d89
Mike Raineri <michael.raineri@dell.com>
2019-06-26 13:21:26 -0400

    Added example using the 'with' statement

---
+++ examples/quickstart_context_mgr.py
@@ -0,0 +1,23 @@
+# Copyright Notice:
+# Copyright 2016-2019 DMTF. All rights reserved.
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/python-redfish-library/blob/master/LICENSE.md
+
+import sys
+import redfish
+
+# When running remotely connect using the address, account name,
+# and password to send https requests
+login_host = "https://192.168.1.100"
+login_account = "admin"
+login_password = "password"
+
+## Create a REDFISH object
+with redfish.redfish_client(base_url=login_host, username=login_account, password=login_password) as REDFISH_OBJ:
+    # Login into the server and create a session
+    REDFISH_OBJ.login(auth="session")
+
+    # Do a GET on a given path
+    response = REDFISH_OBJ.get("/redfish/v1/systems/1", None)
+
+    # Print out the response
+    sys.stdout.write("%s\n" % response)

9e49687b5d695f0573889058cb7be90d55778b46
Mike Raineri <michael.raineri@dell.com>
2019-06-28 08:02:35 -0400

    2.0.9 Versioning

--- src/redfish/__init__.py
+++ src/redfish/__init__.py
@@ -5,7 +5,7 @@
 """ Redfish restful library """
 
 __all__ = ['rest', 'ris', 'discovery']
-__version__ = "2.0.8"
+__version__ = "2.0.9"
 
 from redfish.rest.v1 import redfish_client
 from redfish.rest.v1 import AuthMethod
